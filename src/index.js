4a21b372db172291f58cc6f78f6a0767ffaf14d1
Asieertxo <asgal.gl@gmail.com>
2022-01-19 13:40:22 +0100

    Comenzamos

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*import "./components/Base.scss";
+import "./components/Variables.scss";*/
+
+import Header from './components/Header/Header';
+
+ReactDOM.render(
+  <>
+    <Header />
+
+  </>,
+  document.getElementById('root')
+);
