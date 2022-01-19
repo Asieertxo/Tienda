16e56cdd9f34cfb42bff0a78b1fbdf84153400e6
Asieertxo <asgal.gl@gmail.com>
2022-01-19 23:15:49 +0100

    search apariencia

--- src/components/Header/Search/Search.js
+++ src/components/Header/Search/Search.js
@@ -1,10 +1,14 @@
 import "./Search.scss";
 
+import Lupa from "./../../../img/lupa.png";
+
 function Search() {
   return (
     <form action="#" className="header-search">
         <input type="text" className="header-search__input"></input>
-        <button className="header-search__button">B</button>
+        <button className="header-search__button">
+          <img src={Lupa}></img>
+        </button>
     </form>
   );
 }
