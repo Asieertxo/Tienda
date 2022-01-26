fad2220a4f8db9dd6892d1abc304465ce26ab9b1
Asieertxo <asgal.gl@gmail.com>
2022-01-26 11:11:07 +0100

    imagenes en proceso

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,38 @@
+import mouse from "./../../img/mouse.png";
+import keyboard from "./../../img/keyboard.png";
+import screen from "./../../img/screen.png";
+import arrowrigth from "./../../img/arrow-rigth.png";
+import arrowleft from "./../../img/arrow-left.png";
 
 function Header() {
   return (
     <header className="header">
     <p className="header-T"><b>G</b>aming <b>S</b>hop</p>
     
+
+    <div className="header-choice">
+      <div className="header-choice__images">
+        <p>&lt;</p>
+        <div className="header-choice__images_i">
+          <img src={mouse}></img>
+        </div>
+        <div className="header-choice__images_i">
+          <img src={keyboard}></img>
+        </div>
+        <div className="header-choice__images_i">
+          <img src={screen}></img>
+        </div>
+        <p>&gt;</p>
+      </div>
+      
+    </div>
+
     <div className="header-title">
       <div className="header-bodertop">
         <div className="header-bodertop__circle red"></div>
         <div className="header-bodertop__circle yellow"></div>
         <div className="header-bodertop__circle green"></div>
       </div>
-
       <h1>start renting gaming</h1>
       <h3>best equipment to use in our hotel</h3>
       <div className="header-title__search">
