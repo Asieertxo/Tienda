6cc77499b2fe8718c1d7d35c11826f72a8a0c5fc
Asieertxo <asgal.gl@gmail.com>
2022-02-02 14:03:28 +0100

    secondsection terminado

--- src/components/SecondSection/Item.js
+++ src/components/SecondSection/Item.js
@@ -1,16 +1,42 @@
-
 function Item(props){
     const item = props.item;
 
     return(
         <>
-            <img className="imagen" src={item.img}></img>
-            <span>
+            <img className="second-products__card_imagen" src={item.img}></img>
+            <span className="second-products__card_name">
                 <a href={item.url}>{item.name}</a>
             </span>
-            <span>{item.caract}</span>
-            <span>{item.price}</span>
-            <span>{item.points}</span>
+            <span className="second-products__card_caract">{item.caract}</span>
+            <span className="second-products__card_caract">{item.text}</span>
+            <span className="second-products__card_price">{item.price}€</span>
+            <span className="second-products__card_points">
+                {item.points}
+                {(() => {
+                  switch (item.points) {
+                    case 1:
+                      return (
+                        <div>&#9733;</div>
+                      )
+                    case 2:
+                      return (
+                        <div>&#9733;&#9733;</div>
+                      )
+                    case 3:
+                      return (
+                        <div>&#9733;&#9733;&#9733;</div>
+                      )
+                    case 4:
+                      return (
+                        <div>&#9733;&#9733;&#9733;&#9733;</div>
+                      )
+                    case 5:
+                      return (
+                        <div>&#9733;&#9733;&#9733;&#9733;&#9733;</div>
+                      )
+                  }
+                })()}
+            </span>
         </>
     )
 }
