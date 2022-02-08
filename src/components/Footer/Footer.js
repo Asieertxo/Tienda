df7de2f9e7a43b57d7d9d9960fe5b4af67dfbf03
Asieertxo <asgal.gl@gmail.com>
2022-02-08 17:45:04 +0100

    footer terminado

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,28 @@
+import instagram from "./../../img/icon-instagram.png";
+import facebook from "./../../img/icon-facebook.png";
+import twitter from "./../../img/icon-twitter.png";
+import github from "./../../img/icon-github.png";
+import google from "./../../img/icon-google.png";
+import whatsapp from "./../../img/icon-whatsapp.png";
+
+
 function Footer() {
   
-    return (
-      <>
-
-<footer className="footer">
-      <div className="footer-icons">
-        <i class="fab fa-facebook-f footer-icons_icon"></i>
-      </div>
-      <div className="footer-copyright">
-
-      </div>
-</footer>
-
+  return (
+    <>
+      <footer className="footer">
+        <div className="footer-icons">
+          <img src={instagram}></img>
+          <img src={facebook}></img>
+          <img src={twitter}></img>
+          <img src={github}></img>
+          <img src={google}></img>
+          <img src={whatsapp}></img>
+        </div>
+        <div className="footer-copyright">
+          <p>&#169; 2022 Copyright - TheLan</p>
+        </div>
+      </footer>
       </>
     );
   }
