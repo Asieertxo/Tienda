e3f1791962e41c6158d281c07e048119a7f5f073
Asieertxo <asgal.gl@gmail.com>
2022-02-23 00:25:57 +0100

    refactorizar en reducer

--- src/components/SecondSection/List.js
+++ src/components/SecondSection/List.js
@@ -1,38 +1,66 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useReducer } from "react";
 import Item from "./Item";
 import { getAsyncStories, withError} from "./../../api/bbdd"
+
+const initialStoriesState = {
+  loading: false,
+  error: false,
+  stories: []
+}
+
+const storiesReducer = (state, action) => {
+  switch(action.type){
+    case "STORIES_FETCH__INIT":
+      return{
+        loading: true,
+        error: false,
+        stories: []
+      }
+    case "STORIES_FETCH__SUCCESS":
+      return{
+        loading: false,
+        error: false,
+        stories: action.payload
+      }
+    case "STORIES_FETCH__ERROR":
+      return{
+        loading: false,
+        error: true,
+        stories: []
+      }
+    default:
+      throw Error("action stories error")
+  }
+}
+
+
 
 function List(props) {
 
   useEffect(() => {
     loadStories();
   }, []);
-  const [list, setList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
+
+  const [storiesState, dispatchStories] = useReducer(storiesReducer, initialStoriesState);
+  const { stories, loading, error } = storiesState;
 
   function loadStories(){
-    setIsError(false);
-    setIsLoading(true);
+    dispatchStories({type: "STORIES_FETCH__INIT"})
     withError(getAsyncStories()).then(result => {
-      setList(result.data.stories);
-      setIsLoading(false);
-    }).catch(_err => setIsError(true));
+      dispatchStories({type: "STORIES_FETCH__SUCCESS", payload: result.data.stories})
+    }).catch(_err => dispatchStories({type: "STORIES_FETCH__ERROR"}) );
   }
 
-
-
-
-  if(isError){
+  if(error){
     return <>
       <p>Error!</p>
       <button onClick={loadStories}>Retry</button>
     </>
   }
 
-  return isLoading ? <p>Loading...</p> : 
+  return loading ? <p>Loading...</p> : 
     <div className="second-products">
-      {list.map(function (item){
+      {stories.map(function (item){
         return(
             <div key={item.ObjectID} className="second-products__card">
               <Item item={item} second={true} addCart={props.addCart}/>
